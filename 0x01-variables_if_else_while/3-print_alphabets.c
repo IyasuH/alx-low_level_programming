81dcecd825907427e6daa7835cfecf14cdc8d7bb
IyasuH <eyasuhailegbr@gmail.com>
2021-08-27 11:25:56 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,13 @@
  *Description: LONG
  *Return: return 0
  */
-int main(void){
+int main(void)
+{
 char ch1;
 char ch2;
-for(ch1 = 'a'; ch1 <= 'z'; ch1++)
+for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 putchar(ch1);
-for(ch2 = 'A'; ch2 <= 'Z'; ch2++)
+for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
 putchar(ch2);
 putchar('\n');
 return (0);
