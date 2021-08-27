b33889bf3b4b9a38ed11d0593ff993a88c2714bd
IyasuH <eyasuhailegbr@gmail.com>
2021-08-27 11:18:38 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,9 +7,9 @@
 int main(void){
 char ch1;
 char ch2;
-for(ch1 = 'a', ch2 = 'A'; ch1 <= 'z', ch2 <= 'Z' ; ch1++, ch2++){
+for(ch1 = 'a'; ch1 <= 'z'; ch1++)
 putchar(ch1);
-putchar(ch2);
-}
+for(ch2 = 'A'; ch2 <= 'Z'; ch2++)
+  putchar(ch2);
 return (0);
 }
