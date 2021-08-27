ce1db1d1d20337e4af70699115977cec8e674d1c
IyasuH <eyasuhailegbr@gmail.com>
2021-08-27 10:48:06 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ *main - putchar
+ *Description: pritn alphabet in lowercase using putchar
+ *Return: return null
+ */
+int main(void)
+{
+putchar('a');
+putchar('b');
+putchar('c');
+putchar('d');
+putchar('e');
+putchar('f');
+putchar('g');
+putchar('h');
+putchar('i');
+putchar('j');
+putchar('k');
+putchar('l');
+putchar('m');
+putchar('n');
+putchar('o');
+putchar('p');
+putchar('q');
+putchar('r');
+putchar('s');
+putchar('t');
+putchar('u');
+putchar('v');
+putchar('w');
+putchar('x');
+putchar('y');
+putchar('z');
+return (0);
+}
