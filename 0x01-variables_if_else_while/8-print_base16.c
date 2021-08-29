b57cca5d89879a61df410da50c7f0e7410073fd0
IyasuH <eyasuhailegbr@gmail.com>
2021-08-29 09:44:42 -0700

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,5 +6,16 @@
  */
 int main(void)
 {
-  
+int num;
+char ch;
+for(num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+for(ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
