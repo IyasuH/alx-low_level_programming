2ac9d9a2e4d5e9208065c99bc4b9c6aa4e3f8d9a
IyasuH <eyasuhailegbr@gmail.com>
2021-08-31 13:59:01 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,21 @@
 void print_to_98(int n)
 {
 int a;
+int b;
 if (n > 98)
 {
-for(a = n; a >= 98; a--)
-_putchar(a);
+for (a = n; a >= 98; a--)
+b = a + '0';
+_putchar(b);
+_putchar(' ');
+_putchar('\n');
 }
 else
 {
-for(a = n; a <= 98; a++)
-_putchar(a);
+for (a = n; a <= 98; a++)
+b = a + '0';
+_putchar(b);
+_putchar(' ');
+_putchar('\n');
 }
 }
