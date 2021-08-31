051e914cac1d5dcb2a312e1b50c7f2a02cee7070
IyasuH <eyasuhailegbr@gmail.com>
2021-08-31 11:55:14 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,18 @@
+
 #include "main.h"
 /**
  *print_last_digit - print
+ *@n: int
  *
- *Return: null
+ *Return: int
  */
 int print_last_digit(int n)
 {
 int a;
+if (n < 0)
+a = ((n*-1) % 10);
+else
 a = (n % 10);
+_putchar(a);
 return (a);
 }
