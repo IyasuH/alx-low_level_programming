b514c1018d4ef1bac27fc30f7e28202669a988a2
IyasuH <eyasuhailegbr@gmail.com>
2021-08-31 10:00:06 -0700

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isalpha - check alpha
+ *
+ *Return int
+ */
+int _isalpha(int c)
+{
+int a;
+ if ((c < 123 && c > 96) || (c < 91 && c > 64))
+a = 1;
+else
+a = 0;
+return (a);
+}
