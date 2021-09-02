d6871ea0b726b4e3d90d0ad47d5e355c652e4e74
root <IyasuH>
2021-09-02 06:30:46 -0700

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- *_isdigit - digit or not
+ *_isdigit - mfunc
+ * @c: dgit or not
  *
  *Return: int
  */
 int _isdigit(int c)
 {
-int a ;
+int a;
 if (n >= 0 && n <= 9)
 a = 1;
 else
