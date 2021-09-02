36d6ec99d1a0d295f66696068e56469fe8bf4638
root <IyasuH>
2021-09-01 22:18:24 -0700

    upadet

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * @'\\0/\n"'printf : (description of parameter \\o/\n") 
+ * @'\\0/\n"'printf : (description of parameter \\o/\n")
  *
  * Description: balbal
  * Return: 0
@@ -17,10 +17,10 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 
-//while (i < 10)
-//{
-//putchar(i);
-//}
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
 printf("Infinite loop avoided! \\o/\n");
 
