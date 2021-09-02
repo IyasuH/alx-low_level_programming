c77f68b8c9559669944fc95ece49bd19c18298a0
root <IyasuH>
2021-09-02 09:50:52 -0700

    update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_diagonal - func
+ *@n: int
+ *
+ *Return: null
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
