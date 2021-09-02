59bef15f8010bcf81497fb4bf511cd2d27f40fbe
root <IyasuH>
2021-09-02 07:02:28 -0700

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *print_number - prints num
+ *print_numbers - prints num
  *
  *Return: void
  */
 void print_numbers(void)
 {
 int a;
-for(a = 0; a <= 9; a++)
+for (a = 0; a <= 9; a++)
 _putchar(a + 48);
 _putchar('\n');
 }
