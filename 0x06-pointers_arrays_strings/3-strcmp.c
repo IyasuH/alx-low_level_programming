b2cd240b3eb8e1aa22b9d38e8bdf04d841c465f1
root <IyasuH>
2021-09-09 11:09:59 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,6 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-if (s2 > s1)
-i = -15;
-else if (s1 > s2)
-i = 15;
-else
-i = 0;
+i = strcmp(s1, s2);
 return (i);
 }
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *string_toupper - function
+ *@ch: char to upper
+ *Return: char type
+ */
+char *string_toupper(char *ch)
+{
+return (strupr(ch));
+}
