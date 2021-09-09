a97e100b3d473464de2f4e458c1d4bb602963592
root <IyasuH>
2021-09-08 22:21:09 -0700

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-char *c;
-c = strncpy(dest, src, n);
-return (c);
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+
+return (dest);
 }
