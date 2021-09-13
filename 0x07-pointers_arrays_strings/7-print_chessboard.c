8d946c4f57403a971b42d11dd0157bdd06da384b
root <IyasuH>
2021-09-13 11:59:48 -0700

    update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,13 +14,11 @@ int i;
 int j;
 for (i = 0; i <= 7; i++)
 {
-if (i == 0 || i == 1 || i == 6 || i == 7)
-{
 for (j = 0; j <= 7; j++)
+{
+if (a[i][j] != 0)
 _putchar(a[i][j]);
-_putchar('\n');
 }
-else
 _putchar('\n');
 }
 }
