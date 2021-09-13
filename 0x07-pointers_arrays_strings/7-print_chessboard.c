abb42c878181a2834b64d3c3fdaab2667f616d02
root <IyasuH>
2021-09-13 11:44:33 -0700

    update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,5 +10,17 @@
  */
 void print_chessboard(char (*a)[8])
 {
-  return();
+int i;
+int j;
+for (i = 0; i<=7; i++)
+{
+if(i == 0 || i == 1 || i == 6 || i == 7)
+{
+for(j = 0; j <= 7; j ++)
+_putchar(a[i][j]);
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
 }
---
+++ 0x07-pointers_arrays_strings/_putchar.c~
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *_putchar - func
+ *@c : char
+ *
+ *Return: int type
+ */
+int _putchar(char c)
+{
+  return (writes(1, &c, 1));
+}
