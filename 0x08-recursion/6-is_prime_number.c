9b479e19fac4912cbedd1058054b6d6a0022cf6c
root <IyasuH>
2021-09-15 13:20:51 -0700

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,14 +6,14 @@
  *is_prime_number - function
  *
  *@n: int
- *Return: int type 
+ *Return: int type
  */
 int is_prime_number(int n)
 {
 if (n == 1 || n == -1)
-return(0);
-else if(n % 2 != 0 && n % 3 != 0 && n % 5 != 0)
-return(1);
+return (0);
+else if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0)
+return (1);
 else
-return(0);
+return (0);
 }
