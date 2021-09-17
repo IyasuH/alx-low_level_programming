0825e63c0822c7e0ffd188ea839fadd406a04928
root <IyasuH>
2021-09-17 12:33:16 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
+/**
+ *main - function
+ *
+ *@argc: int
+ *@argv: char
+ *Return: 0
+ */
 int main(int argc, char* argv[])
 {
-if(argc > 0)
+if (argc > 0)
 printf("%s\n", argv[0]);
-return(0);
+return (0);
 }
