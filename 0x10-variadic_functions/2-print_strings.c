7811dd41f5c12b9c56cd14849970196c3e91eaea
root <IyasuH>
2021-09-30 08:25:07 -0700

    update

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *print_strings - function
+ *@separator: char
+ *@n: int
+ *Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+  
+}
