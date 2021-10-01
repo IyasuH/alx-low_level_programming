4c3fbaa0e0ba8eb4a564bbc3624d34c558ed482a
root <IyasuH>
2021-10-01 14:32:36 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <string.h>
 /**
  *print_all - function
  *@format: const char
@@ -9,20 +10,33 @@
 void print_all(const char * const format, ...)
 {
   va_list ap;
-  *char c;
-  int i;
-  *float f;
-  char *s;
   va_start(ap, format);
-  while(va_arg(ap, *char), va_arg(ap, int), va_arg(ap, *float), va_arg(ap, char*))
+  while(*format != '\0')
     {
-      c = va_arg(ap, char);
-      i = va_arg(ap, int);
-      f = va_arg(ap, float);
-      s = va_arg(ap, char*);
-      
-	printf("%c%d%f%s", c,i,f,s);
+      if (*format == 'd') {
+            int i = va_arg(ap, int);
+            printf("%d", i);
+	    if (va_arg(ap, int) < 0)
+	      break;
+        } else if (*format == 'c') {
+            int c = va_arg(ap, int);
+            printf("%c", c);
+	    if (va_arg(ap, int) < 0)
+	      break;
+        } else if (*format == 'f') {
+            double d = va_arg(ap, double);
+            printf("%f", d);
+	    if (va_arg(ap, double) < 0)
+	      break;
+      }else if (*format == 's')
+	{
+	  char *s = va_arg(ap, char*);
+	  printf("%s", s);
+	  if (strlen(va_arg(ap, char*)) == 0)
+	    break;
+	}
       printf(", ");
     }
+  va_end(ap);
   printf("\n");
 }
