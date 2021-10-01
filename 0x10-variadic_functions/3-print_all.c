d9b6862ace7ad7f426a6d780eb3abd71dad4c8ca
root <IyasuH>
2021-10-01 14:39:54 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,34 +9,40 @@
  */
 void print_all(const char * const format, ...)
 {
-  va_list ap;
-  va_start(ap, format);
-  while(*format != '\0')
-    {
-      if (*format == 'd') {
-            int i = va_arg(ap, int);
-            printf("%d", i);
-	    if (va_arg(ap, int) < 0)
-	      break;
-        } else if (*format == 'c') {
-            int c = va_arg(ap, int);
-            printf("%c", c);
-	    if (va_arg(ap, int) < 0)
-	      break;
-        } else if (*format == 'f') {
-            double d = va_arg(ap, double);
-            printf("%f", d);
-	    if (va_arg(ap, double) < 0)
-	      break;
-      }else if (*format == 's')
-	{
-	  char *s = va_arg(ap, char*);
-	  printf("%s", s);
-	  if (strlen(va_arg(ap, char*)) == 0)
-	    break;
-	}
-      printf(", ");
-    }
-  va_end(ap);
-  printf("\n");
+va_list ap;
+va_start(ap, format);
+while (*format != '\0')
+{
+if (*format == 'd')
+{
+int i = va_arg(ap, int);
+printf("%d", i);
+if (va_arg(ap, int) < 0)
+break;
+}
+else if (*format == 'c')
+{
+int c = va_arg(ap, int);
+printf("%c", c);
+if (va_arg(ap, int) < 0)
+break;
+}
+else if (*format == 'f')
+{
+double d = va_arg(ap, double);
+printf("%f", d);
+if (va_arg(ap, double) < 0)
+break;
+}
+else if (*format == 's')
+{
+char *s = va_arg(ap, char*);
+printf("%s", s);
+if (strlen(va_arg(ap, char*)) == 0)
+break;
+}
+printf(", ");
+}
+va_end(ap);
+printf("\n");
 }
