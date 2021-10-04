6c2c1b28767c382d9d561019b7d851f42815c77d
root <IyasuH>
2021-10-04 03:46:46 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
  *new_dog - function
  *@name: char1
@@ -9,5 +10,10 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  
+struct dog *d;
+d = malloc(sizeof(struct dog));
+d->name = name;
+d->age = age;
+d->owner = owner;
+return (d);
 }
