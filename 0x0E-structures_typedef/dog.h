41d23bee831b967bea6c0c970ba2933d65d79a0e
root <IyasuH>
2021-10-04 02:10:42 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG
 #define DOG
+/**
+ *struct dog - stucture
+ *@name: char for entering the dogs age
+ *@age: float for entering the dogs age
+ *@owner: char for entering the owners name
+ */
 struct dog
 {
 char *name;
