1a1f192515528e144b9e742cdba3be641adfd681
root <IyasuH>
2021-10-06 16:36:54 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,17 +3,19 @@
 #include<string.h>
 #include "holberton.h"
 /**
- *_strdup - function that returns a pointer to a newly allocated space in memory 
+ *_strdup - function that returns a pointer to a newly
+ * allocated space in memory
  *which contains copy of the string given as a parameter
  *@str: char
- *return: pointer or NULL
+ *Return: pointer or NULL
  */
 char *_strdup(char *str)
 {
 size_t len = strlen(str) + 1;
 void *new = malloc(len);
-if(new == NULL)
-return NULL;
-return (char *) memcpy (new, str, len);
+if (str != NULL && new != NULL)
+return ((char *) memcpy(new, str, len));
+else
+return (NULL);
 free(new);
 }
