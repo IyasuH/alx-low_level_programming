a7a93606e0a0208c41885e4e14f81fe58d945000
root <IyasuH>
2021-10-06 16:49:23 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,8 +13,10 @@ char *_strdup(char *str)
 {
 size_t len = strlen(str) + 1;
 void *new = malloc(len);
-if (str != NULL)
+if (str != NULL && new != NULL)
 return ((char *) memcpy(new, str, len));
+else if (str != NULL && new == NULL)
+return (NULL);
 else
 return (NULL);
 free(new);
