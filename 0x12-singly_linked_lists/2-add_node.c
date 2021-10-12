bc04e23dcf098ab62c63dca90b6147b5c2df3126
IyasuH <eyasuhailegbr@gmail.com>
2021-10-12 07:40:05 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,11 +13,11 @@ list_t *add_node(list_t **head, char *str1)
 struct list_s *newnode = malloc(sizeof(struct list_s));
 if (newnode != NULL)
 {
-newnode->str = str1;
+
+newnode->str = strdup(str1);
 newnode->next = *head;
 newnode->len = strlen(str1);
 *head = newnode;
-
 return (*head);
 }
 else
