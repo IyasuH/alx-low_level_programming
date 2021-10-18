be59fa93f44eabc971241f7c6b4cd8069e4fd862
IyasuH <eyasuhailegbr@gmail.com>
2021-10-18 08:47:22 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *create_file -  a function that created a file
+ *@filename: const char
+ *@text_content: char
+ *Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int writ;
+int i = 0;
+int fd = open(filename, O_CREAT | O_RDWR, 0600);
+if (text_content)
+{
+while (text_content[i] == '\0')
+i++;
+writ = write(fd, text_content, i);
+}
+if (fd == -1 || filename == NULL || writ == -1)
+{
+close(fd);
+return (-1);
+}
+else
+{
+close(fd);
+return (1);
+}
+}
