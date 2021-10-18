e8405880feaa9f9f1644990ee5a6ce3cd304dad9
IyasuH <eyasuhailegbr@gmail.com>
2021-10-18 07:50:30 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,27 @@
-
-0;136;0c#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 #include <unistd.h>
 #include<fcntl.h>
 
 /**
- *
- *
+ *read_textfile -  a function that reads a text file and
+ *prints it to the POSIX standard output.
+ *@filename: const char
+ *@letters: size_t
+ *Return: ctual number of letters it could read and print or 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd, red, writ;
-  char *buf = malloc(sizeof(char) * letters + 1);
-  fd = open(filename, O_RDONLY);
-  red = read(fd, buf, letters);
-  writ = write(STDOUT_FILENO, buf, red);
-    close(fd);
-    free(buf);
-    if (fd == -1 || filename == NULL || buf == NULL || red == -1 || writ == -1)
-      return (0);
-    else
-      return (writ);
-     }
+int fd, red, writ;
+char *buf = malloc(sizeof(char) * letters + 1);
+fd = open(filename, O_RDONLY);
+red = read(fd, buf, letters);
+writ = write(STDOUT_FILENO, buf, red);
+close(fd);
+free(buf);
+if (fd == -1 || filename == NULL || buf == NULL || red == -1 || writ == -1)
+return (0);
+else
+return (writ);
+}
