bf1f5008f13e367cc5e0542e55ac604045eb6e81
IyasuH <eyasuhailegbr@gmail.com>
2021-10-18 09:09:27 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 int writ;
 int i = 0;
-int fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (text_content)
 {
 while (text_content[i] == '\0')
@@ -19,14 +19,8 @@ writ = write(fd, text_content, i);
 if (writ != i)
 return (-1);
 }
-if (fd == -1 || filename == NULL || writ == -1)
-{
-close(fd);
+if (fd == -1 || filename == NULL)
 return (-1);
-}
-else
-{
 close(fd);
 return (1);
-}
 }
