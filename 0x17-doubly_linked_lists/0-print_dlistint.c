f5021d46cc2f75e87e203a07c0da2de8e68f0217
IyasuH <eyasuhailegbr@gmail.com>
2021-11-25 03:00:42 -0800

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,6 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t a = 0;
-const dlistint_t *tmp = h;
-if (h->next != NULL && h->prev == NULL)
-{
-if (&h->n != NULL)
-{
 while (h != NULL)
 {
 printf("%d\n", h->n);
@@ -23,24 +18,4 @@ h = h->next;
 a++;
 }
 return (a);
-if (tmp == NULL)
-return (1);
-return (0);
-}
-else
-{
-a = 0;
-a += 1;
-while (h != NULL)
-{
-h = h->next;
-a += 1;
-return (a);
-}
-}
-if (tmp == NULL)
-return (1);
-return (0);
-}
-return (1);
 }
