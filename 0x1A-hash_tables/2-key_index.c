9ff674d6ed60042cc7d7b02efe41353fd3a06e6e
IyasuH <eyasuhailegbr@gmail.com>
2021-12-17 05:50:27 -0800

    update

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives the index of a key
+ * @key: the key
+ * @size: the size
+ *
+ * Return: the index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+  unsigned long int index;
+  index = (hash_djb2(key) % size);
+  return (index);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - add an element to hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key 
+ *
+ * Return: 1 if succeeded 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+  
+}
