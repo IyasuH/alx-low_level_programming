fd3d63feaf64dd287fbd3c23fe8500b33fdd5a4c
IyasuH <eyasuhailegbr@gmail.com>
2021-12-17 10:09:57 -0800

    update

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+0;136;0c
 /**
  * key_index - function that gives the index of a key
  * @key: the key
@@ -10,7 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  unsigned long int index;
-  index = (hash_djb2(key) % size);
-  return (index);
+unsigned long int index;
+index = (hash_djb2(key) % size);
+return (index);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,12 +4,35 @@
  * hash_table_set - add an element to hash table
  * @ht: hash table
  * @key: key
- * @value: value associated with the key 
+ * @value: value associated with the key
  *
  * Return: 1 if succeeded 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  
+struct hash_node_s *temp;
+unsigned long int indx;
+temp = malloc(sizeof(hash_node_t));
+if (ht == NULL || key == NULL)
+return (0);
+indx = key_index((const unsigned char *)key, ht->size);
+temp = ht->array[indx];
+while (temp != NULL)
+{
+if (strcmp(temp->key, key) == 0)
+{
+free(temp->value);
+temp->value = strdup(value);
+return (1);
+}
+temp = temp->next;
+}
+if (temp == NULL)
+return (0);
+
+temp->value = strdup(value);
+temp->key = strdup(key);
+ht->array[0] = temp;
+return (1);
 }
