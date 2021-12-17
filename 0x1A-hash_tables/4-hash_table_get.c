d4a5d8a2d6e814b5c02fd2bb8ffa73fef0cd9612
IyasuH <eyasuhailegbr@gmail.com>
2021-12-17 12:57:42 -0800

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - to retrive a value associated with a key
+ * @ht: hash table to look into
+ * @key: key
+ *
+ * Return: value or NULLif key couldn't be found
+ */
+
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+  hash_node_t *temp = NULL;
+  unsigned long int indx;
+
+  if (ht == NULL || key == NULL)
+    return (NULL);
+  while (ht->size > indx)
+    {
+      temp = ht->array[indx];
+      while (temp != NULL)
+	{
+	  /* ifwe find key throuh runing in the table*/
+	  if (strcmp(temp->key, key) == 0)
+	    return (temp->value);
+	}
+      indx++;
+    }
+}
