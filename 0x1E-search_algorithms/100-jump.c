8a52df9ad0b91e1ef8cf542cee6eedd7e4203f6f
IyasuH <eyasuhailegbr@gmail.com>
2022-03-30 19:58:51 -0700

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,12 +2,13 @@
 #include <math.h>
 
 /**
- * jump_serach - searches for a value in sorted array of integers
+ * jump_search - searches for a value in sorted array of integers
  * @array: pointer to first element of sorted Array
  * @size: number of elements in array
  * @value: Is the value to search for
  * Return: first index where value is located
  */
+
 int jump_search(int *array, size_t size, int value)
 {
 	size_t jump = sqrt(size);
@@ -16,34 +17,42 @@ int jump_search(int *array, size_t size, int value)
 	size_t n;
 	size_t max, min;
 
-	if (value == array[size-1])
-        {
-                printf("Value checked array[%ld] = [%d]\n", size-1, array[size-1]);
-                return(size - 1);
+	if (value == array[size - 1])
+	{
+		printf("Value checked array[%ld] = [%d]\n", size - 1, array[size - 1]);
+		return (size - 1);
 	}
-	for (x = 0; x<size; x = x + jump)
+	for (x = 0; x < size; x = x + jump)
 	{
 		if (value > array[x])
 			printf("Value checked array[%ld] = [%d]\n", x, array[x]);
 		else if (value <= array[x])
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", x - jump, x);
-			if (value == array[size-1])
-        		{
-                		printf("Value checked array[%ld] = [%d]\n", size-1, array[size-1]);
-                		return(size - 1);
-        		}
-			for (n = x-jump; n <= x; n++)
+			if (value == array[size - 1])
+			{
+				printf("Value checked array[%ld] = [%d]\n", size - 1, array[size - 1]);
+				return (size - 1);
+			}
+			for (n = x - jump; n <= x; n++)
 			{
 				arr2[n] = array[n];
 			}
-			min = x-jump;
+			min = x - jump;
 			max = x;
 			return (linear_search_me(arr2, max, min, value));
 		}
 	}
 	return (-1);
 }
+/**
+ * linear_search_me - linera search
+ * @array: array to be searched
+ * @max: maximum value to be evaluated
+ * @min: minimum alue to be evaluated
+ * @value: value to be serached
+ * Return: index of the value get
+ */
 
 int linear_search_me(int *array, size_t max, size_t min, int value)
 {
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int iterBinary(int *array, int start_index, int end_index, int value);
+int jump_search(int *array, size_t size, int value);
+int linear_search_me(int *array, size_t max, size_t min, int value);
 
 #endif
